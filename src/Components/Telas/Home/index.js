f669b72db5d691586d7c264b313122417d791c69
Pedro Andrade <86939238+ppedroandrade@users.noreply.github.com>
2022-10-24 23:37:26 -0300

    Iniciando Tela Login

--- src/Components/Telas/Home/index.js
+++ src/Components/Telas/Home/index.js
@@ -1,19 +1,67 @@
 import React from "react";
-import TextField from '@mui/material/TextField';
+import { TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import "./index.css";
 
 function Home() {
-    return (
-    <div>
-        <div class="login-title">
-            <p>Sistemas de Reservas de Espaços</p>            
-        </div>
-        <div class="login-input">
-            <TextField id="outlined-basic" label="Login" variant="outlined" />
-            <TextField id="outlined-basic" label="Senha" variant="outlined" />
-
-        </div>
+  return (
+    <div className="login-screen">
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          sx={{
+            width: 541,
+            height: 575,
+            backgroundColor: "#FFFFFF",
+            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexWrap: "wrap",
+            flexDirection: "column",
+            alignContent: "space-between",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: 40,
+          }}
+        >
+          <p class="login-title">Sistemas de Reservas de Espaços</p>
+          <TextField
+            id="outlined-basic"
+            label="Informe seu Login"
+            variant="outlined"
+            className="input-login"
+            style={{ marginLeft: 30,marginBottom: 36, marginTop: 41, width: 465, height: 45 }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Informe sua Senha"
+            variant="outlined"
+            className="input-passowd"
+            style={{ marginLeft: 30, width: 465, height: 45 }}
+          />
+          <Button
+            variant="contained"
+            style={{
+              width: 465,
+              height: 45,
+              marginBottom: 36,
+              marginTop: 36,
+              marginLeft: 30,
+              backgroundColor: "#01e78e",
+            }}
+          >
+            Acessar
+          </Button>
+          <Button variant="outlined" style={{ width: 465, height: 45,marginLeft: 30 }}>
+            Esqueci minha senha
+          </Button>
+        </Box>
+      </Grid>
     </div>
-    );
-};
+  );
+}
 export default Home;
