315559bfb3573ba501f42fc28f72ef911707e655
Kauê Sanches <kauesanches123@gmail.com>
2022-10-26 17:55:03 -0300

    Ajuste dos cards

--- src/Components/Telas/Inicitial/index.js
+++ src/Components/Telas/Inicitial/index.js
@@ -8,17 +8,21 @@ function Inicitial() {
     <div>
       <Header />
       <div className="container">
+        <div className="cima">
         <div className="box-auditorio">
           <p>Auditorio</p>
         </div>
         <div className="box-lab">
           <p>Laboratorios</p>
         </div>
+        </div>
+        <div className="baixo">
         <div className="box-class-room-small">
           <p>Salas de Aula pequena</p>
         </div>
         <div className="box-class-room">
           <p>Salas de Aula</p>
+        </div>
         </div>
       </div>
     </div>
