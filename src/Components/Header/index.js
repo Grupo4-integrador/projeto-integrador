eefbe49599042dd4065a345bc601c2aaa9d023fe
Pedro Andrade <86939238+ppedroandrade@users.noreply.github.com>
2022-10-26 16:11:40 -0300

    teste

---
+++ src/Components/Header/index.js
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import { Add } from "@mui/icons-material";
+
+function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar style={{ background: "#fff" }} position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            style={{ color: "#000" }}
+          >
+            <MenuIcon sx={{ fontSize: "35px" }} />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            style={{ color: "#000" }}
+          >
+            <AccountCircle sx={{ fontSize: "35px" }} />
+          </IconButton>
+
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Perfil</MenuItem>
+            <MenuItem onClick={handleClose}>Alterar sua Senha</MenuItem>
+            <Link style={{ textDecoration: "none" }} to="/">
+              <MenuItem onClick={handleClose}>Sair</MenuItem>
+            </Link>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header;
--- src/Components/Telas/Inicitial/index.js
+++ src/Components/Telas/Inicitial/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+import Header from "../../Header";
+
 function Inicitial() {
   return( 
-  <p>Tela Inicial</p>
+    <div>
+      <Header />
+
+      <h1>Sou o resto da tela</h1>
+    </div>
   );
 }
 export default Inicitial;
