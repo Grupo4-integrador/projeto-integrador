27610dfe60ae5a6cbbca6ce90a61d234f1efff49
Pedro Andrade <86939238+ppedroandrade@users.noreply.github.com>
2022-10-28 04:47:53 -0300

    tela inicial quase pronta

--- src/Components/Telas/Inicitial/index.js
+++ src/Components/Telas/Inicitial/index.js
@@ -1,35 +1,14 @@
 import React from "react";
 
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import { Link } from "react-router-dom";
 
 
-
-import Header from "../../Header";
 import "./index.css";
+import SidebarHome from "../../SideBar";
 
 function Inicitial() {
   return (
     <div>
-      <Header />
-
-      <div style={{ display: 'flex', height: '100%' }}>
-      <Sidebar>
-        <Menu>
-          <MenuItem>Suas Reservas</MenuItem>
-          <MenuItem>Editar Reservas</MenuItem>
-          <MenuItem>Falar com secretaria</MenuItem>
-          <MenuItem>Reportar um problema de Reserva</MenuItem>
-          <MenuItem>Lista de Materiais</MenuItem>
-          <MenuItem>Solicitar equipamento para aula</MenuItem>
-          <MenuItem>Equipamentos utilizados na aula passada</MenuItem>
-          <Link style={{ textDecoration: "none" }} to="/">
-          <MenuItem>Sair do sistema</MenuItem>
-          </Link>
-        </Menu>
-      </Sidebar>
-    </div>
-
+      <SidebarHome />
       <div className="container">
         <div className="cima">
         <div className="box-auditorio">
