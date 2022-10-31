088180db6939293c33b818a522a100283376f11f
Pedro Andrade <86939238+ppedroandrade@users.noreply.github.com>
2022-10-31 18:56:39 -0300

    tela inicial pronta

--- src/Components/Telas/Inicitial/index.js
+++ src/Components/Telas/Inicitial/index.js
@@ -1,33 +1,95 @@
 import React from "react";
 
-
+import Box from "@mui/material/Box";
 
 import "./index.css";
 import SidebarHome from "../../SideBar";
 
+
 function Inicitial() {
   return (
-    <div>
+      <div>
       <SidebarHome />
-      <div className="container">
-        <div className="cima">
-        <div className="box-auditorio">
-          <p>Auditorio</p>
-        </div>
-        <div className="box-lab">
-          <p>Laboratorios</p>
-        </div>
-        </div>
-        <div className="baixo">
-        <div className="box-class-room-small">
-          <p>Salas de Aula pequena</p>
-        </div>
-        <div className="box-class-room">
-          <p>Salas de Aula</p>
-        </div>
+        <div className="container">
+          <Box  style={{ backgroundColor: "white", opacity: "0.5" }}
+            sx={{
+              width: 600,
+              height: 400,
+              backgroundImage:'url(/Img/Auditorio.jpg)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'fixed',
+              backgroundRepeat: 'no-repeat',
+              boxShadow: "4px 4px 4px 4px rgba(0, 0, 0, 0.25)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            <p>Auditório</p>
+          </Box>
+
+          <Box  style={{ backgroundColor: "white", opacity: "0.5" }}
+            sx={{
+              width: 600,
+              height: 400,
+              backgroundImage:'url(/Img/sala-aula.jpeg)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'fixed',
+              backgroundRepeat: 'no-repeat',
+              boxShadow: "4px 4px 4px 4px rgba(0, 0, 0, 0.25)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            <p>Salas de Aula</p>
+          </Box>
+
+          <Box  style={{ backgroundColor: "white", opacity: "0.5" }}
+            sx={{
+              width: 600,
+              height: 400,
+              backgroundImage:'url(/Img/laboratorio.jpg)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'fixed',
+              backgroundRepeat: 'no-repeat',
+              boxShadow: "4px 4px 4px 4px rgba(0, 0, 0, 0.25)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "5px",
+              marginTop: "20px",
+              cursor: "pointer",
+            }}
+            >
+            <p>Laboratórios</p>
+          </Box>
+
+          <Box  style={{ backgroundColor: "white", opacity: "0.5" }}
+            sx={{
+              width: 600,
+              height: 400,
+              backgroundImage:'url(/Img/reuniao.jpg) ',
+              backgroundSize: 'cover',
+              backgroundPosition: 'fixed',
+              backgroundRepeat: 'no-repeat',
+              boxShadow: "4px 4px 4px 4px rgba(0, 0, 0, 0.25)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "5px",
+              marginTop: "20px",
+              cursor: "pointer",
+            }}
+            >
+              <p>Salas de Reunião</p>
+          </Box>
         </div>
       </div>
-    </div>
   );
 }
 export default Inicitial;
