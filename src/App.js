0fb53387df1493b23d18ac5324c1d00e7f943d31
Pedro Andrade <86939238+ppedroandrade@users.noreply.github.com>
2022-11-22 20:00:21 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Telas/Home";
 import Inicitial from "./Components/Telas/Inicitial/index";
 import FogetPassword from "./Components/Telas/Foget-password";
+import Lab from "./Components/Telas/Laboratorio";
+import ClassRoom from "./Components/Telas/SalaDeAula";
+import Auditorio from "./Components/Telas/Auditorio";
+import Meeting from "./Components/Telas/SalaDeReuniao";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Route index element={<Home />} />
         <Route path="home" element={<Inicitial />} />
         <Route path="password-forgot" element={<FogetPassword />} />
+        <Route path="/auditorio" element={<Auditorio />} />
+        <Route path="/saladeaula" element={<ClassRoom />} />
+        <Route path="/reuniao" element={<Meeting />} />
+        <Route path="/laboratorio" element={<Lab />} />
       </Routes>
     </div>
   );
